5670cc05e0618293ba3af58aa6880b037d35bb49
Richard Steinbach <steinbach.dick@gmail.com>
2019-07-08 19:19:27 -0700

    add danerouslySetInnerHTML to Story component

--- src/components/Story.js
+++ src/components/Story.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 function Story(props) {
 
+  function createMarkup(){
+    return {__html: props.story.content.fields.body}
+  }
+
     if(props.story){
-      let storyBody = props.story.content.fields.body
-  return (
-    <div className="story">
-    <p>dangerouslySetInnerHtml={storyBody}</p>
-    </div>
-  )}
-  else {return props.story}
+      //let storyBody = props.story.content.fields.body
+      return (
+        <div className="story" dangerouslySetInnerHTML={createMarkup()}>
+
+        </div>
+      )}else {return props.story}
 }
 
 export default Story;
