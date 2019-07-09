92ce7557bf77fcb4a8efaaf825e6909fa97a78d4
Richard Steinbach <steinbach.dick@gmail.com>
2019-07-08 19:33:00 -0700

    set Politics and General components to display HTML from API

--- src/components/General.js
+++ src/components/General.js
@@ -1,14 +1,33 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 function General(props){
-  let list = []
+
+  let listLink = []
+
   if(props.world){
-    list = props.world.map(function(head) {return <p> <a href={head.webUrl}>{head.webTitle}</a></p>})}
+    //list = props.sports.map(function(head) {return <p> <a href={head.webUrl}>{head.webTitle}</a></p>})
+
+    listLink = props.world.map(function(head) {
+
+      return <p><Link to={{
+            pathname: `/story/2}`,
+            state: `${head.apiUrl}`
+          }}>{head.webTitle}
+          </Link></p>
+      //head.apiUrl
+        }
+      )
+    }
+
   return(
     <div>
 
-      {list}
+      {listLink}
     </div>
   )
 }
 export default General;
+
+// let list = []
+// if(props.world){
+//   list = props.world.map(function(head) {return <p> <a href={head.webUrl}>{head.webTitle}</a></p>})}
--- src/components/Politics.js
+++ src/components/Politics.js
@@ -1,16 +1,27 @@
 import React from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Politics(props){
-console.log(process.env.API_KEY)
-  let list = []
+
+  let listLink = []
+
   if(props.politics){
-  list = props.politics.map(function(head) {return <p> <a href={head.apiUrl + "?&show-fields=all&api-key=" + process.env.REACT_APP_API_KEY}>{head.webTitle}</a></p>})}
+    //list = props.sports.map(function(head) {return <p> <a href={head.webUrl}>{head.webTitle}</a></p>})
 
+    listLink = props.politics.map(function(head) {
 
+      return <p><Link to={{
+            pathname: `/story/2}`,
+            state: `${head.apiUrl}`
+          }}>{head.webTitle}
+          </Link></p>
+      //head.apiUrl
+        }
+      )
+    }
   return(
     <div>
-      {list}
+      {listLink}
     </div>
 
   )
